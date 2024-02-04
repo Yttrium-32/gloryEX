c158b137286ed622a3827aa6ad9ce7f8696e6c1b
Yttrium_32 <shresthprasad7@gmail.com>
2024-02-05 01:55:18 +0530

    created files for widgets

---
+++ gui/cards.py
@@ -0,0 +1,12 @@
+from PySide6.QtWidgets import QWidget
+
+class CardWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        pass
+
+class CardImageWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        pass
+
---
+++ gui/login_page.py
@@ -0,0 +1,7 @@
+from PySide6.QtWidgets import QWidget
+
+class LoginPage(QWidget):
+    def __init__(self):
+        super().__init__()
+        pass
+
