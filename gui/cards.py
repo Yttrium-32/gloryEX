acee017c581c510d84607921c883d8cd3da67779
Yttrium_32 <shresthprasad7@gmail.com>
2024-02-06 02:25:34 +0530

    add full implementation of text and image cards

--- gui/cards.py
+++ gui/cards.py
@@ -1,5 +1,24 @@
-from PySide6.QtCore import QSize
-from PySide6.QtWidgets import QApplication, QLabel, QVBoxLayout, QWidget
+from PySide6.QtCore import QSize, Qt
+from PySide6.QtGui import QPixmap
+from PySide6.QtWidgets import QApplication, QHBoxLayout, QLabel, QSizePolicy, QVBoxLayout, QWidget
+
+from os import PathLike
+from pathlib import Path
+
+CARD_TITLE_STYLE = """
+    background-color: #8e44ad;
+    border-radius: 10px;
+    color: #fff;
+    font-size: 18pt;
+    font-weight: bold;
+"""
+
+CARD_BODY_STYLE = """
+    padding: 15px;
+    border-radius: 10px;
+    background-color: #9b59b6;
+    color: #333;
+"""
 
 class CardWidget(QWidget):
     def __init__(self, title: str, content: str) -> None:
@@ -9,11 +28,15 @@ class CardWidget(QWidget):
         self.setMaximumSize(QSize(500, 500))
 
         card_title = QLabel(title)
-        card_title.setMaximumWidth(300)
+        card_title.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        card_title.setAlignment(Qt.AlignCenter)
+        card_title.setMaximumHeight(60)
+        card_title.setStyleSheet(CARD_TITLE_STYLE)
 
         card_body = QLabel(content)
         card_body.setMaximumWidth(500)
         card_body.setWordWrap(True)
+        card_body.setStyleSheet(CARD_BODY_STYLE)
 
         card_layout = QVBoxLayout()
         card_layout.addWidget(card_title)
@@ -22,13 +45,48 @@ class CardWidget(QWidget):
         self.setLayout(card_layout)
 
 class CardImageWidget(QWidget):
-    def __init__(self):
+    def __init__(self, title: str, content: str, image_path: PathLike):
         super().__init__()
-        pass
+        self.setWindowTitle("Text Card with Image")
+        self.setMinimumSize(QSize(300, 300))
+        self.setMaximumSize(QSize(500, 500))
+
+        card_title = QLabel(title)
+        card_title.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        card_title.setAlignment(Qt.AlignCenter)
+        card_title.setMaximumHeight(60)
+        card_title.setStyleSheet(CARD_TITLE_STYLE)
+
+        content = QLabel(content)
+        content.setMaximumWidth(500)
+        content.setWordWrap(True)
+        content.setStyleSheet(CARD_BODY_STYLE)
+
+        image_label = QLabel()
+        image_label.setPixmap(QPixmap(image_path))
+        image_label.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+
+        card_body_layout = QHBoxLayout()
+        card_body_layout.addWidget(content)
+        card_body_layout.addWidget(image_label)
+
+        card_layout = QVBoxLayout()
+
+        card_layout.addWidget(card_title)
+        card_layout.addLayout(card_body_layout)
+
+        self.setLayout(card_layout)
 
 if __name__ == "__main__":
     app = QApplication()
+
     body = "This is a long a complex body used for testing in a card body."
+    image_path = Path("../assets/logo_128x128.png")
+
     card_widget = CardWidget("Test Title", body)
     card_widget.show()
+
+    image_card_widget = CardImageWidget("Test Title", body, image_path.resolve())
+    image_card_widget.show()
+
     app.exec()
